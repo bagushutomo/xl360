9f97c1e8849867117bfafc97e2ad1bb98210cd7a
Bagus Hutomo <bagus.hutomo@gmail.com>
2015-08-18 13:03:55 +0700

    compiled source

--- js/release.js
+++ js/release.js
@@ -36929,7 +36929,6 @@ THREE.StereoEffect = function ( renderer ) {
       containerHi = document.getElementById(sceneIDHi);
       containerLo = document.getElementById(sceneIDLo);
 
-      // cameraHi = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
       cameraHi = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
       cameraHi.target = new THREE.Vector3( 0, 0, 0 );
       cameraLo = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
@@ -36967,54 +36966,6 @@ THREE.StereoEffect = function ( renderer ) {
       window.addEventListener('resize', onWindowResize, false);
       window.addEventListener('deviceorientation', setOrientationControls, true);
 
-      /*
-      effect = new THREE.StereoEffect(renderer);
-
-      scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera(90, 1, 0.001, 700);
-      camera.position.set(0, 10, 0);
-      scene.add(camera);
-
-      controls = new THREE.OrbitControls(camera, element);
-      controls.rotateUp(Math.PI / 4);
-      controls.target.set(
-        camera.position.x + 0.1,
-        camera.position.y,
-        camera.position.z
-      );
-      controls.noZoom = true;
-      controls.noPan = true;
-
-      var light = new THREE.HemisphereLight(0x777777, 0x000000, 0.6);
-      scene.add(light);
-      
-      texture = THREE.ImageUtils.loadTexture(texturePath);
-
-      texture.wrapS = THREE.RepeatWrapping;
-      texture.wrapT = THREE.RepeatWrapping;
-      texture.repeat = new THREE.Vector2(50, 50);
-      texture.anisotropy = renderer.getMaxAnisotropy();
-
-
-      var material = new THREE.MeshPhongMaterial({
-        color: 0xffffff,
-        specular: 0xffffff,
-        shininess: 20,
-        shading: THREE.FlatShading,
-        map: texture
-      });
-
-      var geometry = new THREE.PlaneGeometry(1000, 1000);
-
-      var mesh = new THREE.Mesh(geometry, material);
-      mesh.rotation.x = -Math.PI / 2;
-      scene.add(mesh);
-
-      window.addEventListener('resize', resize, false);
-      setTimeout(resize, 1);
-
-      // window.addEventListener('deviceorientation', setOrientationControls, true);
-      */
     },
 
     start: function(t) {
